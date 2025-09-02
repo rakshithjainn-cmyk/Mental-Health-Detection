ce71c00f8a3857c206762d6427d9c6db52f15bcd
rakshithjainn-cmyk <rakshithjainn@gmail.com>
2025-09-03 00:01:23 +0530

    Update and rename api.py to app/api.py

--- app/api.py
+++ app/api.py
@@ -1,26 +1,19 @@
-# app/api.py
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.inference import predict
 
-
 app = FastAPI(title='Mental Health Detection API')
 
-
 class TextIn(BaseModel):
-text: str
-
+    text: str
 
 class PredictionOut(BaseModel):
-label_id: int
-label_name: str
-probs: list
-
+    label_id: int
+    label_name: str
+    probs: list
 
 @app.post('/predict', response_model=PredictionOut)
 async def predict_endpoint(payload: TextIn):
-res = predict(payload.text)
-return res
+    return predict(payload.text)
 
-
-# Run: uvicorn app.api:app --host 0.0.0.0 --port 8000
+# Run with: uvicorn app.api:app --host 0.0.0.0 --port 8000
